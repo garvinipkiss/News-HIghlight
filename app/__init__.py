7ba5c7ced8d26954c0c093b45e17ebf966d4be55
garvinipkiss <garvinipkiss@gmail.com>
2018-04-08 22:29:30 +0300

    created an instance

--- app/__init__.py
+++ app/__init__.py
@@ -1,2 +1,31 @@
 from flask import Flask
 from config import config_options
+# from flask_bootstrap import Bootstrap
+
+
+def create_app(config_name):
+    """
+     connecting our app.
+    """
+
+    app = Flask(__name__)
+
+    # Setting up Configurations
+    app.config.from_object(config_options[config_name])
+    # app.config.from_pyfile('config.py')
+
+    # Registering the blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Setting up the request config
+    from .request import configure_request
+    configure_request(app)
+
+    # Initializing Flask Extension
+    # bootstrap = Bootstrap(app)
+
+    # TODO: Will add the view
+    # from app import views
+
+    return app
