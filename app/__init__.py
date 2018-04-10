86f16132e87e15f24757d13fd29e9a870930dad4
garvinipkiss <garvinipkiss@gmail.com>
2018-04-10 10:35:15 +0300

    updated source

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,23 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from config import config_options
-# from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 
 def create_app(config_name):
-    """
-     connecting our app.
-    """
+  app = Flask(__name__)
 
-    app = Flask(__name__)
+  #app configurations
+  app.config.from_object(config_options[config_name])
+  #flask extensions
+  bootstrap.init_app(app)
 
-    # Setting up Configurations
-    app.config.from_object(config_options[config_name])
-    # app.config.from_pyfile('config.py')
+  #registering blueprint
+  from .main import main as main_blueprint
+  app.register_blueprint(main_blueprint)
 
-    # Registering the blueprint
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
+  #setting config
+  from .request import configure_request
+  configure_request(app)
 
-    # Setting up the request config
-    from .request import configure_request
-    configure_request(app)
-
-    # Initializing Flask Extension
-    # bootstrap = Bootstrap(app)
-
-    # TODO: Will add the view
-    # from app import views
-
-    return app
+  return app
